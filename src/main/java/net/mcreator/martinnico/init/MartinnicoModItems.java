deaab8624592e9ab134df0c6f45da031e92c678f
nicop451 <101954494+nicop451@users.noreply.github.com>
2022-03-20 12:55:15 -0400

    Vine Grapple Gun

---
+++ src/main/java/net/mcreator/martinnico/init/MartinnicoModItems.java
@@ -0,0 +1,32 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.martinnico.init;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.item.Item;
+
+import net.mcreator.martinnico.item.TreeSwingerItem;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MartinnicoModItems {
+	private static final List<Item> REGISTRY = new ArrayList<>();
+	public static final Item TREE_SWINGER = register(new TreeSwingerItem());
+
+	private static Item register(Item item) {
+		REGISTRY.add(item);
+		return item;
+	}
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		event.getRegistry().registerAll(REGISTRY.toArray(new Item[0]));
+	}
+}
